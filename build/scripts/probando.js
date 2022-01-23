118bb38344996c5848814f66ca55c07b3d8b1407
Ramiro <ramegp@gmail.com>
2022-01-23 19:07:28 -0300

    Subido desde node

--- build/scripts/probando.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let obj = {};
-if (Object.keys(obj).length == 0) {
-    console.log("no tiene");
-}
-else {
-    console.log("Tiene");
-}
-//# sourceMappingURL=probando.js.map
--- build/utils/AbstracClass.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HandlePersistenciaProductos {
-}
-exports.HandlePersistenciaProductos = HandlePersistenciaProductos;
-//# sourceMappingURL=AbstracClass.js.map
